17f56fc1a9be14db4372888aa2f417c6397093d8
Nicolas Bermudez <bermnicolas@gmail.com>
2019-02-27 18:06:23 +0000

    Automatically backed up by Learn

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,16 @@
 class Ship
+  attr_accessor :name, :weight, :height
+
+  @@all = []
+
+  def initialize(params)
+    @name = params[:name]
+    @weight = params[:weight]
+    @height = params[:height]
+  end
+
+  def self.all
+    @@all
+  end
+
 end
